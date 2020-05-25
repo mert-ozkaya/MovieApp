c919285f38fa586587457a10dd59d46ab12275a7
Mert Ozkaya <40736764+mert-ozkaya@users.noreply.github.com>
2020-05-26 01:26:07 +0300

    Create ErrorModel.swift

---
+++ MovieApp/Models/ErrorModel.swift
@@ -0,0 +1,21 @@
+//
+//  ErrorModel.swift
+//  MovieApp
+//
+//  Created by Mert Ozkaya on 24.05.2020.
+//  Copyright © 2020 Mert Ozkaya. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+class ErrorModel {
+    var status_message: String
+    var status_code: Int
+    
+    init(json: JSON) {
+        status_message = json["status_message"].stringValue
+        status_code = json["status_code"].intValue
+    }
+    
+}
