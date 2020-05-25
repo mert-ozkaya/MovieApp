5e48dbf4218b28b27a35bff0b9ca9d87e4732288
Mert Ozkaya <40736764+mert-ozkaya@users.noreply.github.com>
2020-05-26 01:30:23 +0300

    Create BaseService.swift

---
+++ MovieApp/Services/BaseService.swift
@@ -0,0 +1,13 @@
+//
+//  BaseService.swift
+//  MovieApp
+//
+//  Created by Mert Ozkaya on 25.05.2020.
+//  Copyright © 2020 Mert Ozkaya. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+
+
